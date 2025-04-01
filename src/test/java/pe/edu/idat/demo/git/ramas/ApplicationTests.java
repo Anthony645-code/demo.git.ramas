55f8a2de41ba0c3a83ea71944aedbbdf22825167
Anthony645 <tony_645@outlook.com>
2025-04-01 08:05:04 -0500

    proyecto base

---
+++ src/test/java/pe/edu/idat/demo/git/ramas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo.git.ramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
